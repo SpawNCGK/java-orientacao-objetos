2ec4cbf292c6adcde264220597e1cce03c3bec65
Augusto Cesar <gutold.cgkever@gmail.com>
2026-03-08 23:47:04 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,1 +1,16 @@
 
+package main;
+
+import classes.Pessoa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pessoa p = new Pessoa();
+        p.nome = "Augusto";
+        p.idade = 30;
+
+        p.apresentar();
+    }
+}
