1fd40e5fd70128287585f5003c1d2738c7026d21
Augusto Cesar <gutold.cgkever@gmail.com>
2026-03-08 23:25:26 -0300

    Update Pessoas.java

--- src/classes/Pessoas.java
+++ src/classes/Pessoas.java
@@ -1,1 +1,13 @@
 
+package classes;
+
+public class Pessoa {
+
+    String nome;
+    int idade;
+
+    public void apresentar() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
