d6f862d99b088105fc21b6b4ff1c9433466a9500
Augusto Cesar <gutold.cgkever@gmail.com>
2026-03-08 23:26:11 -0300

    Update ContaBancaria.java

--- src/encapsulamento/ContaBancaria.java
+++ src/encapsulamento/ContaBancaria.java
@@ -1,1 +1,15 @@
 
+package encapsulamento;
+
+public class ContaBancaria {
+
+    private double saldo;
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
